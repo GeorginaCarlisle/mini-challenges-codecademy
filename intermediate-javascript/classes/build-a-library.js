88dc9de0797b7869a9cad1d2359b0a800ea2b621
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-07-21 21:06:43 +0100

    FEAT: Add isCheckouted out property and functionsality to Media class

--- intermediate-javascript/classes/build-a-library.js
+++ intermediate-javascript/classes/build-a-library.js
@@ -1,10 +1,19 @@
 class Media {
   constructor(title) {
     this._title = title;
+    this._isCheckedOut = false;
   }
 
   get title() {
     return this._title;
+  }
+
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
   }
 }
 
--- intermediate-javascript/classes/library.test.js
+++ intermediate-javascript/classes/library.test.js
@@ -11,4 +11,20 @@ describe('Media parent class', () => {
     const newMedia = new Media("title");
     expect(newMedia.title).toBe("title");
   });
+
+  test('Constructs isCheckedOut property initially set to false', () => {
+    const newMedia = new Media("title");
+    expect(newMedia._isCheckedOut).toBe(false);
+  });
+
+  test('isCheckedOut getter returns isCheckedOut property', () => {
+    const newMedia = new Media("title");
+    expect(newMedia.isCheckedOut).toBe(false);
+  });
+
+  test('toggleCheckOutStatus method switches isCheckedOut', () => {
+    const newMedia = new Media("title");
+    newMedia.toggleCheckOutStatus();
+    expect(newMedia.isCheckedOut).toBe(true);
+  });
 });
